761b95422ea069c35fc12ed545a7a021d1f3430e
jayfoxfoxy <joao.p.s.pinho@gmail.com>
2019-11-01 23:06:01 +0000

    Added ArrayManipulation template

---
+++ BasicTemplate.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class BasicTemplate{
+
+   public static void main(String[] args){   
+   }
+      
+   static void Menu() {
+   }
+     
+}
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ARU-JAVA-TEMPLATES
 
+JAVA template with basic template (Main and Class)
+
 JAVA template with basic menu.
 
 JAVA template with basic array manipulation (vector)
